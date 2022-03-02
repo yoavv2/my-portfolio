f9fc87c318eefda91bb376c8a747b0347cc12e6d
yoav <23hutc8@gmail.com>
2022-03-03 01:28:35 +0200

    delete useMobile hook

--- utils/useIsMobile.tsx
+++
@@ -1,6 +0,0 @@
-import { useUserAgent } from '@teambit/ui-foundation.ui.hooks.use-user-agent';
-
-export function useIsMobile() {
-  const deviceType = useUserAgent()?.getDevice().type;
-  return deviceType === 'tablet' || deviceType === 'mobile';
-}
