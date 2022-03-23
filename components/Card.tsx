fc92a6d4fe1d2837f2f516f939f9eb3ed473017e
yoav <23hutc8@gmail.com>
2022-03-24 00:26:58 +0200

    .

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,6 +1,6 @@
 import { format, parseISO } from 'date-fns';
 import Link from 'next/link';
-import { motion } from 'framer-motion';
+
 import Rive from 'rive-react';
 const Card = ({
   name,
@@ -19,7 +19,7 @@ const Card = ({
     <>
       {/* bg-[#17141d]  */}
 
-      <motion.article
+      <article
         className='min-w-32 lex-col shadow-3xl hover:divide-x-84 relative clear-both flex h-[450px] w-96
          cursor-pointer flex-col rounded-3xl bg-yellow-100 p-6  transition-transform hover:-translate-y-5 
          focus-within:hover:-translate-y-10 focus-within:hover:-translate-x-3 dark:bg-slate-800 dark:shadow-gray-700 '
@@ -71,12 +71,11 @@ const Card = ({
               src='/rive/avatar1.riv'
               className='filter-shadow-lg absolute bottom-4 block h-10 w-10 overflow-hidden rounded-full bg-white'
             />
-            {/* absolute -bottom-2 left-[10px] */}
+
             <svg
               className='stroke-linecap-round stroke-linejoin-round pointer-events-none  h-[48px] w-[60px] fill-transparent stroke-[#ff8a00] stroke-2'
               viewBox='0 0 106 57'
             >
-              {/* absolute bottom-0 top-8 */}
               <path d='M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4'></path>
             </svg>
           </span>
@@ -101,7 +100,7 @@ const Card = ({
             </a>
           ))}
         </div>
-      </motion.article>
+      </article>
     </>
   );
 };
