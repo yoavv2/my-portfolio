f744b0503a26cb963029bda4f997d69a7dd51a7b
yoavv2 <23hutc8@gmail.com>
2023-03-09 17:05:20 +0200

    .

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -26,7 +26,7 @@ const showSomethingWentWrong = () => (
     <Link href='https://github.com/yoavv2'>
       <a
         target='_blank'
-        className='group  relative my-10 ml-4 inline-flex items-center justify-start overflow-hidden rounded-full px-5 py-3 font-mono font-bold'
+        className='group relative my-10 ml-4 inline-flex items-center justify-start overflow-hidden rounded-full px-5 py-3 font-mono font-bold'
       >
         <span className='absolute left-0 top-0 h-32 w-32 translate-x-12 -translate-y-2 rotate-45 bg-slate-300 opacity-[3%] dark:bg-white'></span>
         <span className='absolute top-0 left-0 -mt-1 h-48 w-48 -translate-x-56 -translate-y-24 rotate-45 bg-red-300 opacity-100 transition-all duration-500 ease-in-out group-hover:-translate-x-8 dark:bg-amber-300'></span>
