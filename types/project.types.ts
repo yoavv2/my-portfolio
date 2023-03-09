961a977857271ad24a1508bc8222739ba4335b51
yoavv2 <23hutc8@gmail.com>
2023-03-09 17:17:23 +0200

    .

--- types/project.types.ts
+++ types/project.types.ts
@@ -2,10 +2,10 @@ export interface IProject {
   name: string;
   html_url: string;
   created_at: string;
-  updated_at: string;
   language: string;
   languages: string[];
   description?: string;
+  updated_at?: string;
   homepage?: string;
   stars?: string;
   readme?: any;
