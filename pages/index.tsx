abec55edfc5033adb11caab96b221c0aee34ec23
yoavv2 <23hutc8@gmail.com>
2024-05-11 12:35:33 +0300

    update: about me

--- pages/index.tsx
+++ pages/index.tsx
@@ -71,12 +71,12 @@ const Home = (): JSX.Element => {
       {/* top-52 */}
       <RiveComponent
         onClick={() => clickOnMarty()}
-        className='absolute right-0 h-32 cursor-pointer 3xl:left-36 top-28 w-28 sm:h-60 sm:w-60 lg:top-14 lg:h-96 lg:w-96'
+        className='3xl:left-36 absolute right-0 top-28 h-32 w-28 cursor-pointer sm:h-60 sm:w-60 lg:top-14 lg:h-96 lg:w-96'
       />
       <motion.div variants={container} initial='hidden' animate='visible'>
         <motion.div variants={item}>
           <header>
-            <h2 className='font-mono font-extrabold  lg:text-5xl'>
+            <h2 className='font-mono font-extrabold lg:text-5xl'>
               <span
                 style={{
                   color: `${getColor()}`,
@@ -110,50 +110,53 @@ const Home = (): JSX.Element => {
             <h3 className='font-mono text-3xl'>I’m a Full-stack Developer.</h3>
           </header>
         </motion.div>
-        <p className='mt-20 font-mono text-2xl  lg:text-3xl'>
-          I’m very comfortable working with JavaScript, HTML and CSS.
+        <p className='mt-20 font-mono text-xl lg:text-xl'>
+          I love creating user-friendly websites and web apps that are both
+          functional and visually appealing. I have a strong understanding of
+          JavaScript, HTML, CSS, and the MERN stack (MongoDB, Express, React,
+          Node.js). I'm especially skilled at using React.js to build
+          interactive and user-friendly interfaces. I also have a keen eye for
+          detail and a deep understanding of UI/UX principles, which allows me
+          to create intuitive and visually appealing solutions that users love.
           <br />
-          Building and maintaining web applications and dashboards using React
-          or NextJS.
+          I'm a team player and I'm always learning new things. I'm also a big
+          fan of agile methodologies. I thrive in fast-paced environments where
+          I can continuously learn and adapt to emerging technologies and
+          industry trends.
           <br />
           <br />
-          Looking to grow as a fullstack / frontend developer in a company
+          I'm always looking for new challenges and opportunities to push the
+          boundaries of what's possible in web development. Whether it's
+          optimizing existing applications or building groundbreaking solutions
+          from scratch, I approach every project with enthusiasm and a
+          relentless pursuit of quality.
           <br />
-          that puts UI/UX at the top of the list.
+          <br />
+          I'm currently looking for a challenging role in a forward-thinking
+          company that values creativity, teamwork, and a dedication to
+          delivering exceptional user experiences. Let's collaborate and create
+          something truly remarkable together.
         </p>
-        <ul
-          role='list'
-          className='grid grid-cols-1 gap-4 p-0 m-0 text-xl list-none list-outside lg:text-2xl'
-        >
-          <li className='flex justify-start gap-2 before:text-xl before:content-["👉"] before:lg:text-3xl'>
-            My Stack is Mostly Typescript, React with Next, MongoDB with
-            Mongoose, Node with Express and Im Experienced with GraphQL with
-            Apollo, firebase.
-          </li>
-          <li className='flex justify-start gap-2 before:text-xl before:content-["👉"] before:lg:text-3xl'>
-            B.Sc. in Mathematics and Computer Science.
-          </li>
-        </ul>
 
         <h2 className='my-10 font-mono '>Here's how you can contact me!</h2>
       </motion.div>
 
       <div
-        // className='flex flex-col items-center justify-center mb-20 space-y-2 border-2  sm:flex-row sm:items-start sm:justify-start sm:space-x-2 sm:space-y-0'
-        className='flex mb-20 space-x-2 text-sm sm:text-base'
+        // className='flex flex-col items-center justify-center mb-20 space-y-2 border-2 sm:flex-row sm:items-start sm:justify-start sm:space-x-2 sm:space-y-0'
+        className='mb-20 flex space-x-2 text-sm sm:text-base'
       >
         <Link href='https://www.linkedin.com/in/yoavhevroni/'>
           <a
             target='_blank'
             aria-label='linkedin'
-            className='relative inline-flex items-center justify-start px-5 py-3 overflow-hidden font-mono font-bold rounded-full group'
+            className='group relative inline-flex items-center justify-start overflow-hidden rounded-full px-5 py-3 font-mono font-bold'
           >
             <span className='absolute left-0 top-0 h-32 w-32 translate-x-12 -translate-y-2 rotate-45 bg-slate-300 opacity-[3%] dark:bg-white'></span>
-            <span className='absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-indigo-400 opacity-100 group-hover:-translate-x-8 dark:bg-lime-300'></span>
+            <span className='absolute top-0 left-0 -mt-1 h-48 w-48 -translate-x-56 -translate-y-24 rotate-45 bg-indigo-400 opacity-100 transition-all duration-500 ease-in-out group-hover:-translate-x-8 dark:bg-lime-300'></span>
             <span className='relative w-full text-center text-teal-900 transition-colors duration-200 ease-in-out dark:text-white dark:group-hover:text-gray-900'>
               linkedin
             </span>
-            <span className='absolute inset-0 border-2 border-black rounded-full dark:border-white'></span>
+            <span className='absolute inset-0 rounded-full border-2 border-black dark:border-white'></span>
           </a>
         </Link>
 
@@ -161,14 +164,14 @@ const Home = (): JSX.Element => {
           <a
             target='_blank'
             aria-label='github'
-            className='relative inline-flex items-center justify-start flex-shrink px-5 py-3 overflow-hidden font-mono font-bold rounded-full group'
+            className='group relative inline-flex flex-shrink items-center justify-start overflow-hidden rounded-full px-5 py-3 font-mono font-bold'
           >
             <span className='absolute left-0 top-0 h-32 w-32 translate-x-12 -translate-y-2 rotate-45 bg-gray-300 opacity-[3%] dark:bg-white'></span>
-            <span className='absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 opacity-100 bg-emerald-400 group-hover:-translate-x-8 dark:bg-cyan-300'></span>
+            <span className='absolute top-0 left-0 -mt-1 h-48 w-48 -translate-x-56 -translate-y-24 rotate-45 bg-emerald-400 opacity-100 transition-all duration-500 ease-in-out group-hover:-translate-x-8 dark:bg-cyan-300'></span>
             <span className='relative w-full text-center text-teal-900 transition-colors duration-200 ease-in-out dark:text-white dark:group-hover:text-gray-900'>
               github
             </span>
-            <span className='absolute inset-0 border-2 border-black rounded-full dark:border-white'></span>
+            <span className='absolute inset-0 rounded-full border-2 border-black dark:border-white'></span>
           </a>
         </Link>
 
@@ -176,14 +179,14 @@ const Home = (): JSX.Element => {
           <a
             target='_blank'
             aria-label='mail'
-            className='relative inline-flex items-center justify-start px-5 py-3 overflow-hidden font-mono font-bold rounded-full group'
+            className='group relative inline-flex items-center justify-start overflow-hidden rounded-full px-5 py-3 font-mono font-bold'
           >
             <span className='absolute left-0 top-0 h-32 w-32 translate-x-12 -translate-y-2 rotate-45 bg-gray-300 opacity-[3%] dark:bg-white'></span>
-            <span className='absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-yellow-400 opacity-100 group-hover:-translate-x-8 dark:bg-orange-300'></span>
+            <span className='absolute top-0 left-0 -mt-1 h-48 w-48 -translate-x-56 -translate-y-24 rotate-45 bg-yellow-400 opacity-100 transition-all duration-500 ease-in-out group-hover:-translate-x-8 dark:bg-orange-300'></span>
             <span className='relative w-full text-center text-teal-900 transition-colors duration-200 ease-in-out dark:text-white dark:group-hover:text-gray-900'>
               email
             </span>
-            <span className='absolute inset-0 border-2 border-black rounded-full dark:border-white'></span>
+            <span className='absolute inset-0 rounded-full border-2 border-black dark:border-white'></span>
           </a>
         </Link>
       </div>
