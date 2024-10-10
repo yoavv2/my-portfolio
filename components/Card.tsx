cb3777b76639007335b47195d1a949a00a47252d
yoavv2 <23hutc8@gmail.com>
2024-10-11 02:57:35 +0300

    refactor(CardList): integrate Rive avatar and update card details Beta

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,94 +1,97 @@
 import React from 'react';
-import Link from 'next/link';
-import Rive from '@rive-app/react-canvas';
 import { format, parseISO } from 'date-fns';
 import { getColor } from '../utils/color.util';
+import Link from 'next/link';
+import Rive from '@rive-app/react-canvas';
+
+interface CardProps {
+  name: string;
+  html_url: string;
+  description: string;
+  languages: string[];
+  language: string;
+  created_at: string;
+  homepage?: string; // Optional
+  setRepoName: (repoName: string) => void;
+}
 
 const RIVE_AVATAR = '/rive/avatar1.riv';
 
-const CardList = ({ cards, setRepoName }) => {
+const Card: React.FC<CardProps> = ({
+  name,
+  html_url,
+  description,
+  languages,
+  created_at,
+  setRepoName,
+  homepage,
+}) => {
   return (
-    <section className='scrollbar-thin scrollbar-thumb-[#201c29] scrollbar-track-[linear-gradient(90deg,#201c29,#201c29_1px,#17141d_0,#17141d)] flex p-12 overflow-x-scroll'>
-      {cards.map((card, index) => (
-        <article
-          key={index}
-          className='relative flex flex-col h-[450px] w-96 min-w-[250px] p-6 rounded-3xl bg-yellow-100 shadow-[-1rem_0_3rem_#000] transition-transform duration-200 ml-[-130px] first:ml-0 hover:translate-y-[-1rem] focus-within:hover:translate-x-[130px] dark:bg-[#17141d] dark:shadow-[#17141d]'
-          onClick={() => setRepoName(card.name)}
+    <article
+      className='card h-[350px] w-[400px] min-w-[250px] p-6 rounded-xl bg-[#17141d] shadow-[-1rem_0_3rem_#000] flex flex-col transition-all duration-200 relative focus-within:hover:translate-x-[130px] hover:translate-y-[-1rem] first:ml-0 ml-[-130px]'
+      onClick={() => setRepoName(name)}
+    >
+      <header className='card-header mb-auto'>
+        <p className='text-sm text-[#7a7a8c]'>
+          {format(parseISO(created_at), 'MMMM dd, yyyy')}
+        </p>
+        <h2 className='text-lg mt-1 mb-0 cursor-pointer hover:bg-gradient-to-r from-[#ff8a00] to-[#e52e71] hover:text-transparent hover:bg-clip-text transition-all'>
+          {name.split('-').join(' ')}
+        </h2>
+        <p className='text-md mt-1 font-mono'>{description}</p>
+        {homepage && (
+          <a
+            href={homepage}
+            target='_blank'
+            rel='noopener noreferrer'
+            className='block mt-4 font-mono text-sm bg-clip-text text-[#7a7a8c] uppercase hover:bg-gradient-to-r hover:from-orange-400 hover:to-red-700 hover:text-transparent hover:underline transition-all'
+          >
+            VIEW PROJECT
+          </a>
+        )}
+        <a
+          href={html_url}
+          target='_blank'
+          rel='noopener noreferrer'
+          className='block mt-2 font-mono text-sm bg-clip-text text-[#7a7a8c] uppercase hover:bg-gradient-to-r hover:from-orange-400 hover:to-red-700 hover:text-transparent hover:underline transition-all'
         >
-          <header className='flex-grow mb-auto'>
-            <p className='text-sm text-[#7a7a8c]'>
-              {format(parseISO(card.created_at), 'MMMM dd, yyyy')}
-            </p>
-            <h2 className='font-mdm my-1 ml-auto text-xl bg-gradient-to-r bg-clip-text fill-transparent hover:from-orange-400 hover:to-red-700 hover:text-transparent'>
-              {card.name.split('-').join(' ')}
-            </h2>
-            <p className='text-md font-mono bg-gradient-to-r bg-clip-text fill-transparent hover:from-orange-400 hover:to-red-700 hover:text-transparent'>
-              {card.description}
-            </p>
-            {card.homepage && (
-              <Link href={card.homepage}>
-                <a
-                  target='_blank'
-                  className='cardTag flex items-center justify-center w-1/2 ml-4 font-mono text-sm bg-gradient-to-r bg-clip-text fill-transparent hover:from-orange-400 hover:to-red-700 hover:text-transparent hover:underline '
-                  data-splitbee-event='View project'
-                  data-splitbee-event-destination={card.name
-                    .split('-')
-                    .join(' ')}
-                >
-                  VIEW PROJECT
-                </a>
-              </Link>
-            )}
-            <Link href={card.html_url}>
-              <a
-                target='_blank'
-                className='cardTag flex items-center justify-center w-1/2 ml-4 font-mono text-sm cursor-pointer bg-gradient-to-r bg-clip-text fill-transparent hover:from-orange-400 hover:to-red-700 hover:text-transparent hover:underline'
-                data-splitbee-event='View code'
-                data-splitbee-event-destination={card.name.split('-').join(' ')}
-              >
-                VIEW CODE
-              </a>
-            </Link>
-          </header>
+          VIEW CODE
+        </a>
+      </header>
+      <div className='relative flex items-center h-20'>
+        <span className='box-border relative flex flex-col items-center justify-center '>
+          <Rive
+            src={RIVE_AVATAR}
+            className='filter-shadow-lg absolute block w-10 h-10 overflow-hidden bg-white rounded-full bottom-4'
+          />
+          <svg
+            className='stroke-linecap-round stroke-linejoin-round pointer-events-none h-[48px] w-[60px] fill-transparent stroke-[#ff8a00] stroke-2'
+            viewBox='0 0 106 57'
+          >
+            <path d='M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4'></path>
+          </svg>
+        </span>
 
-          <div className='relative flex items-center h-20'>
-            <span className='box-border relative flex flex-col items-center justify-center '>
-              <Rive
-                src={RIVE_AVATAR}
-                className='filter-shadow-lg absolute block w-10 h-10 overflow-hidden bg-white rounded-full bottom-4'
-              />
-              <svg
-                className='stroke-linecap-round stroke-linejoin-round pointer-events-none h-[48px] w-[60px] fill-transparent stroke-[#ff8a00] stroke-2'
-                viewBox='0 0 106 57'
-              >
-                <path d='M102 4c0 27.1-21.9 49-49 49S4 31.1 4 4'></path>
-              </svg>
-            </span>
+        <div className='font-mdm mb-3 ml-3 bg-gradient-to-r bg-clip-text fill-transparent hover:from-orange-400 hover:to-red-700 hover:text-transparent'>
+          <div className=' font-extrabold text-[#7a7a8c]'>Author</div>
+          Yoav Hevroni
+        </div>
+      </div>
 
-            <div className='font-mdm mb-3 ml-3 bg-gradient-to-r bg-clip-text fill-transparent hover:from-orange-400 hover:to-red-700 hover:text-transparent'>
-              <div className=' font-extrabold text-[#7a7a8c]'>Author</div>
-              Yoav Hevroni
-            </div>
-          </div>
-
-          <div className='text-md flex flex-wrap px-0 pt-2 pb-4 mx-4 mt-4 font-bold leading-4 uppercase '>
-            {Object.keys(card.languages).map((lang) => (
-              <a
-                className='cardTag mb-1'
-                href='#'
-                key={lang}
-                style={{
-                  backgroundColor: `${getColor()}`,
-                }}
-              >
-                {lang}
-              </a>
-            ))}
-          </div>
-        </article>
-      ))}
-    </section>
+      <div className='tags flex flex-wrap mt-4'>
+        {Object.keys(languages).map((lang) => (
+          <a
+            key={lang}
+            href='#'
+            className='block mb-1 text-[#7a7a8c] uppercase font-bold text-xs border border-[#28242f] rounded-full px-3 py-1 mr-2 hover:bg-gradient-to-r hover:from-[#ff8a00] hover:to-[#e52e71] hover:text-transparent hover:border-white transition-all'
+            style={{ backgroundColor: getColor() }}
+          >
+            {lang}
+          </a>
+        ))}
+      </div>
+    </article>
   );
 };
 
-export default CardList;
+export default Card;
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import useSWR from 'swr';
 import Link from 'next/link';
 import { NextSeo } from 'next-seo';
-import Card from '../components/Card';
 import ReactMarkdown from 'react-markdown';
 import { IProject } from '../types/project.types';
 import Rive from '@rive-app/react-canvas';
+import Card from '../components/Card';
 
 const url = 'https://site-yoavv2.vercel.app/';
 const title = "Yoav's Portfolio";
 const description = 'Projects and Experiences';
-// const image = 'https://site-yoavv2.vercel.app/static/images/yoav-profile.jpg';
 
 export async function fetcher<JSON = Record<string, unknown>>(
   input: RequestInfo,
@@ -26,14 +25,14 @@ const showSomethingWentWrong = () => (
     <Link href='https://github.com/yoavv2'>
       <a
         target='_blank'
-        className='group relative my-10 ml-4 inline-flex items-center justify-start overflow-hidden rounded-full px-5 py-3 font-mono font-bold'
+        className='group relative inline-flex items-center justify-start px-5 py-3 my-10 ml-4 overflow-hidden font-mono font-bold rounded-full'
       >
         <span className='absolute left-0 top-0 h-32 w-32 translate-x-12 -translate-y-2 rotate-45 bg-slate-300 opacity-[3%] dark:bg-white'></span>
-        <span className='absolute top-0 left-0 -mt-1 h-48 w-48 -translate-x-56 -translate-y-24 rotate-45 bg-red-300 opacity-100 transition-all duration-500 ease-in-out group-hover:-translate-x-8 dark:bg-amber-300'></span>
+        <span className='absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-red-300 opacity-100 group-hover:-translate-x-8 dark:bg-amber-300'></span>
         <span className='relative w-full text-left text-teal-900 transition-colors duration-200 ease-in-out dark:text-white dark:group-hover:text-gray-900'>
           github
         </span>
-        <span className='absolute inset-0 rounded-full border-2 border-black dark:border-white'></span>
+        <span className='absolute inset-0 border-2 border-black rounded-full dark:border-white'></span>
       </a>
     </Link>
   </div>
@@ -49,13 +48,7 @@ const Projects = () => {
     });
   }, [setIsSmall]);
 
-  const {
-    data, // default value is []
-    //if data is undefined or null or empty array otherwise data is assigned to projects
-    // variable and projects is assigned to data.projects which is an array of projects type (ProjectType)
-
-    error,
-  } = useSWR<IProject[]>('/api/github', fetcher, {
+  const { data, error } = useSWR<IProject[]>('/api/github', fetcher, {
     revalidateOnFocus: false,
     refreshInterval: 0,
     refreshWhenHidden: false,
@@ -89,17 +82,13 @@ const Projects = () => {
           url,
           title,
           description,
-          // images: [image],
           site_name: 'Yoav Hevroni Portfolio',
         }}
       />
       <h1 className='font-mdm flex items-center justify-center'> Projects </h1>
 
       {!!data.length && (
-        <ul
-          className='mx-auto flex flex-col items-center justify-center
-                    overflow-x-scroll rounded-xl sm:flex-row sm:justify-start sm:p-12'
-        >
+        <ul className='flex flex-col items-center justify-center mx-auto overflow-x-scroll rounded-xl sm:flex-row sm:justify-start sm:p-12'>
           {data?.map((project: IProject) => (
             <li key={project.name}>
               <Card
@@ -117,10 +106,9 @@ const Projects = () => {
         </ul>
       )}
       {data.length > 0 ? (
-        <div className=' flex justify-center'>
+        <div className='flex justify-center '>
           {!isSmall && !!repoName.length && (
             <article className='shadow-3xl my-10 flex min-w-full flex-col items-start rounded-lg border border-dashed border-b-slate-500 bg-yellow-100  p-4 dark:border-white dark:bg-[#17141d] dark:shadow-[#17141d]'>
-              {/* find the repository readme by the name */}
               <ReactMarkdown>
                 {data?.find((repo: any) => repo.name == repoName)?.readme}
               </ReactMarkdown>
